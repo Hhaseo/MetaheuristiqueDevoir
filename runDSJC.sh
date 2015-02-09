982cea6b772d0481c609558156f6e1778d5b2517
elnabo <g.desquesnes@gmail.com>
2015-02-10 00:17:09 +0100

    Updated run scripts

--- runDSJC.sh
+++ runDSJC.sh
@@ -1,14 +1,14 @@
 sh build.sh
 
-python run.py DSJC1000.1.col 1
-python run.py DSJC1000.5.col 5
-python run.py DSJC1000.9.col 9
-python run.py DSJC125.1.col 1
-python run.py DSJC125.5.col 5
-python run.py DSJC125.9.col 9
-python run.py DSJC250.1.col 1
-python run.py DSJC250.5.col 5
-python run.py DSJC250.9.col 9
-python run.py DSJC500.1.col 1
-python run.py DSJC500.5.col 5 
-python run.py DSJC500.9.col 9
+python run.py examples/DSJC1000.1.col 1
+python run.py examples/DSJC1000.5.col 5
+python run.py examples/DSJC1000.9.col 9
+python run.py examples/DSJC125.1.col 1
+python run.py examples/DSJC125.5.col 5
+python run.py examples/DSJC125.9.col 9
+python run.py examples/DSJC250.1.col 1
+python run.py examples/DSJC250.5.col 5
+python run.py examples/DSJC250.9.col 9
+python run.py examples/DSJC500.1.col 1
+python run.py examples/DSJC500.5.col 5 
+python run.py examples/DSJC500.9.col 9
--- runle.sh
+++ runle.sh
@@ -1,13 +1,13 @@
 sh build.sh
 
-python run.py le450_15b.col 15
-python run.py le450_15c.col 15
-python run.py le450_15d.col 15
-python run.py le450_25a.col 25 
-python run.py le450_25b.col 25
-python run.py le450_25c.col 25
-python run.py le450_25d.col 25
-python run.py le450_5a.col 5
-python run.py le450_5b.col 5
-python run.py le450_5c.col 5
-python run.py le450_5d.col 5
+python run.py examples/le450_15b.col 15
+python run.py examples/le450_15c.col 15
+python run.py examples/le450_15d.col 15
+python run.py examples/le450_25a.col 25 
+python run.py examples/le450_25b.col 25
+python run.py examples/le450_25c.col 25
+python run.py examples/le450_25d.col 25
+python run.py examples/le450_5a.col 5
+python run.py examples/le450_5b.col 5
+python run.py examples/le450_5c.col 5
+python run.py examples/le450_5d.col 5
