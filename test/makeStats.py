3b043d592cb10d146192602bbd090c2572b0e53b
Florian Benavent <Florian.Benavent@gmail.com>
2015-02-10 11:34:45 +0100

    Fin des tests

--- test/makeStats.py
+++ test/makeStats.py
@@ -11,7 +11,7 @@ for s in outputs:
 		lines = f.readlines()
 		graph_id = s[4:-6]+"+COLOR"+s[-5]
 		conflicts[graph_id] += 0.5 * float(lines[-6].split()[-1])
-		duration[graph_id]  += 0.5 * float(lines[-4].split()[-1])
+		duration[graph_id]  += float(lines[-4].split()[-1])
 
 with open("conflicts.txt",'w') as f:
 	f.write("file->conflict->duration\n")
